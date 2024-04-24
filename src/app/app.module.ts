7c230875efe0eb03bd450c1396e4d93358e91632
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-24 20:50:55 +0200

    test auto deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"vasgergo-b3370","appId":"1:657290616852:web:e7b0e0f8fad9118dfe0701","storageBucket":"vasgergo-b3370.appspot.com","apiKey":"AIzaSyAP9Xn13R_hVnnR8z4R1TgyVq-dTd2rVtE","authDomain":"vasgergo-b3370.firebaseapp.com","messagingSenderId":"657290616852","measurementId":"G-7T4QF91JBB"})),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
